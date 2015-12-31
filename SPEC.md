e626188aaecc6557e19d8118bb2d2a35fccb9c51
gytdau <gytdau@gmail.com>
2015-12-31 19:01:09 +0000

    Added conditional statements (if, or if, or  else, end)
    See SPEC.md for more information about this change.

--- SPEC.md
+++ SPEC.md
@@ -33,3 +33,47 @@ Ask "What is a?" for a
 ```
 
 Asks the user the question `"What is a?"` and puts the user's response into `a`
+
+
+## Conditionals
+
+This is the general pattern for conditionals:
+```
+If conditional:
+    <code>
+Or if conditional:
+    <code>
+Or else:
+    <code>
+End
+```
+
+
+Below, you will find the conversion chart for Tome syntax into normal programming syntax for conditionals.
+
+| Syntax in Tome  | Normal syntax |
+|-----------------|---------------|
+|   is equal to   |       ==      |
+| is not equal to |       !=      |
+| is greater than |       >       |
+|   is less than  |       <       |
+|       and       |       &&      |
+|        or       |       \|\|      |
+
+This is a sample conditional program, to show you the syntax required. Parenthesies are allowed but discouraged.
+```
+If a is equal to 5:
+Show "A is 5"
+Or if a is equal to 4:
+Show "A is 4"
+Or else:
+Show "A is not 5 or 4"
+End
+```
+```
+Discouraged syntax:
+If (a is equal to 5):
+
+Encouraged syntax:
+If a is equal to 5:
+```
--- interpreter.js
+++ interpreter.js
@@ -26,9 +26,37 @@ function execute() {
             matches = /Ask (".+") for (.+)/g.exec(x[i]);
             addLineToScript("var " + matches[2] + " = prompt(" + matches[1] + ")")
         }
+        else if(line[0] == "If") {
+            matches = /If (.+):/g.exec(x[i]);
+            addLineToScript("if(" + parseConditionalExpression(matches[1]) + ") {")
+        }
+        else if(line[0] == "End") {
+            addLineToScript('}')
+        }
+        else if(line[1] == "else:") {
+            addLineToScript('} else {')
+        }
+        else if(line[1] == "if") {
+            matches = /Or if (.+):/g.exec(x[i]);
+            addLineToScript("} else if(" + parseConditionalExpression(matches[1]) + ") {")
+        }
     }
-
+    console.log(script);
     new Function(script)();
+}
+
+function parseConditionalExpression(expression) {
+    expression = replaceAll(expression, " is equal to ", " == ");
+    expression = replaceAll(expression, " is not equal to ", " != ");
+    expression = replaceAll(expression, " is greater than ", " > ");
+    expression = replaceAll(expression, " is less than ", " < ");
+    expression = replaceAll(expression, " and ", " && ");
+    expression = replaceAll(expression, " or ", " || ");
+    return expression;
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
 }
 
 function addLineToScript(newLine) {
